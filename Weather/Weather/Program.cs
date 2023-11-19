11aaa506bd61ef47c1212176e9ea8d27205537ee
Konstantin2702 <klaptev15337@gmail.com>
2023-11-19 22:57:29 +0300

    feat: Настроил Program, конфиг и проектный файл

--- Weather/Weather/Program.cs
+++ Weather/Weather/Program.cs
@@ -1,27 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using Weather.Models;
+using Weather.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<WeatherContext>(options => options.UseSqlServer(connection));
+builder.Services.AddMemoryCache();
+builder.Services.AddTransient<IWorkWithFiles, WorkWithFiles>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
 
+app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html"); ;
+app.MapControllers();
 
 app.Run();
