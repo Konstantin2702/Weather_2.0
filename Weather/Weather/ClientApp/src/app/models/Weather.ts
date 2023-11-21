0e8c0be4d8de1a6d57395c8d090f40d93cd0eb2d
klaptev15337@gmail.com <klaptev15337@gmail.com>
2023-11-21 19:24:53 +0300

    style: Переместил

---
+++ Weather/Weather/ClientApp/src/app/models/Weather.ts
@@ -0,0 +1,19 @@
+export class Weather {
+  constructor(
+    public id: String,
+    public dateOfTaking: String,
+    public timeOfTaking: String,
+    public temparature: String,
+    public humidity: String,
+    public dewPoint: String,
+    public pressure: String,
+    public windDirection: String,
+    public windSpeed: String,
+    public cloudiness: String,
+    public cloudBase: String,
+    public horizontalVisibility: String,
+    public weatherCondition: String
+  ) {
+
+  }
+}
