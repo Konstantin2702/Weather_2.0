213e76f9191e09297ecaaa9bd320b7919c0f3e7a
klaptev15337@gmail.com <klaptev15337@gmail.com>
2023-11-21 19:24:30 +0300

    refactor: Вынес все методы взаимодейтсвия с сервером в DataService

--- Weather/Weather/ClientApp/src/app/services/data.service.ts
+++ Weather/Weather/ClientApp/src/app/services/data.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
-
+import { FileWithInfo } from '../models/FileWithInfo';
 
 @Injectable({ providedIn: 'root' })
 export class DataService {
-
-  private url = "/api/weather";
-
   constructor(private http: HttpClient) {
   }
 
   getWeather(month: number, year: number, pageNumber: number, countOFElementsOnPage: number) {
-    let tempUrl = "api/Home/api/weather/GetWeather"
+    if (month === null) month = 0;
+    if (year === null) year = 0;
+    let tempUrl = "api/getWeather"
     return this.http.get(tempUrl + "?" + "month=" + month + "&year=" + year + "&pageNumber=" +
       pageNumber + "&countOFElementsOnPage=" + countOFElementsOnPage);
   }
 
   getCountOfElementsToShow(month: number, year: number) {
-    let tempUrl = "api/Home/api/weather/GetCountWeather";
+    if (month === null) month = 0;
+    if (year === null) year = 0;
+    let tempUrl = "api/count";
     return this.http.get(tempUrl + "?" + "month=" + month + "&year=" + year);
+  }
+
+  saveData(file: FileWithInfo) {
+    let fd = new FormData();
+    file.isLoading = true;
+    file.info = "";
+    fd.append('files', file.file, file.file.name);
+    return this.http.post('api/send', fd);
   }
 }
